cc85f3944fff07a61603e9cc1d1571319fcd0afe
Bright Okafor <smartme783@gmail.com>
2020-04-21 11:21:21 +0100

    deugging destructured properties

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,15 @@
 const handleSignin = (db, bcrypt) =>(res, req)=> {
-
+const {email, password} = req.body;
   if (!email || !password){
     return res.status(400).json('Incorrect form submission')
   }
   db.select('email','hash').from('login')
-  .where('email', '=', email.req.body)
+  .where('email', '=', email)
   .then(data => {
-  	const isValid= bcrypt.compareSync(password.req.body, data[0].hash);
+  	const isValid= bcrypt.compareSync(password, data[0].hash);
   	if(isValid){
   		return db.select('*').from('users')
-  		.where('email', '=', email.req.body)
+  		.where('email', '=', email)
   		.then(user=> {
   			res.json(user[0])
   		})
