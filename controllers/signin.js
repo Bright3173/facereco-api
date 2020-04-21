d93de78c51be806a4d549237c872cde79f01bcd0
Bright Okafor <smartme783@gmail.com>
2020-04-21 14:45:50 +0100

    deugging destructured properties

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,4 @@
-const handleSignin = (db, bcrypt) =>(res, req)=> {
+const handleSignin = (db, bcrypt) =>(req, res)=> {
 const {email, password} = req.body;
   if (!email || !password){
     return res.status(400).json('Incorrect form submission')
