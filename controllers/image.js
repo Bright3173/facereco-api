8446bdf6390856fd057cd8ffa0eea668091ed677
Bright Okafor <smartme783@gmail.com>
2020-04-25 12:50:51 +0100

    fixing bug

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-// apiKey: 'Insert your Clarifai apiKey Here'
+e187d983d7584ba99fddcbf58d29c5b7
 });
 
 const handleApiCall = (req, res) => {
