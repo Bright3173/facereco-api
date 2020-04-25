f4a6543a712130c8300c1169e0753a29c0e3021e
Bright Okafor <smartme783@gmail.com>
2020-04-25 14:11:34 +0100

    fixing bug

--- controllers/register.js
+++ controllers/register.js
@@ -27,7 +27,8 @@ const handleRegister = (req, res, db, bcrypt) => {
 		.then(trx.commit)
 		.catch(trx.rollback)
 	})
-	.catch(err=> res.status(400).json('Cannot Register User'))
+	.catch(err=>{console.log(err) res.status(400).json('Cannot Register User'))}
+	
 	}
 
 	module.exports = {
